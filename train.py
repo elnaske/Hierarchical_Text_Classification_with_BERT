da439b69708f0eb071b5aa7e4bc932756f632227
elnaske <naske.elias@outlook.com>
2025-01-06 18:20:29 -0500

    Add training for all tiers

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
-from transformers import RobertaTokenizer, RobertaForSequenceClassification, RobertaModel, TrainingArguments, Trainer
+from transformers import RobertaTokenizer, RobertaForSequenceClassification, TrainingArguments, Trainer
 import torch
 from torch import nn
 
-from dataset import download_dataset, parse_dataset, get_classes, Tier1Dataset
-from trainer_utils import compute_metrics_t1, save_model
+from dataset import download_dataset, parse_dataset, get_classes, Tier1Dataset, Tier3Dataset
+from trainer_utils import compute_metrics_t1, compute_metrics, get_mask_matrix, save_model, HierarchicalDataCollator
+from model import HierarchicalBERT
 
 def train_t1(train_set, eval_set):
-    classes, class_to_idx, idx_to_class, taxonomy = get_classes(train_set)
+    classes, class_to_idx, idx_to_class, _ = get_classes(train_set)
 
     model_name = "FacebookAI/roberta-base"
     model = RobertaForSequenceClassification.from_pretrained(model_name, num_labels = len(classes['tier1']))
@@ -21,8 +22,8 @@ def train_t1(train_set, eval_set):
     print('Tokenization finished')
 
     training_args = TrainingArguments(output_dir = 'roberta-t1-genres',
-                                         eval_strategy = 'epoch',
-                                         save_strategy = 'epoch')
+                                      eval_strategy = 'epoch',
+                                      save_strategy = 'epoch')
     
     trainer_t1 = Trainer(
         model = model,
@@ -39,13 +40,67 @@ def train_t1(train_set, eval_set):
 
     return model, tokenizer
 
+def train(train_set, eval_set):
+    classes, class_to_idx, idx_to_class, taxonomy = get_classes(train_set)
+
+    # Convert the taxonomy to matrices designating the parent-child relationships between classes
+    mask_mat_t2 = get_mask_matrix(taxonomy['tier1'], class_to_idx['tier1'], class_to_idx['tier2'])
+    mask_mat_t3 = get_mask_matrix(taxonomy['tier1'], class_to_idx['tier2'], class_to_idx['tier3'])
+
+    # Instantiate the model and tokenizer
+    model_name = "FacebookAI/roberta-base"
+    print(f'Instantiating model from {model_name}...')
+    model = HierarchicalBERT.from_pretrained(model_name,
+                                             num_labels = len(classes['tier1']),
+                                             num_tier2 = len(classes['tier2']),
+                                             num_tier3 = len(classes['tier3']),
+                                             mask_tier2 = mask_mat_t2,
+                                             mask_tier3 = mask_mat_t3)
+    print(f'Instantiating tokenizer from {model_name}...')
+    tokenizer = RobertaTokenizer.from_pretrained(model_name)
+
+    # Converting classes to integers
+    y1_train = [class_to_idx[c] for c in train_set['tier1']]
+    y2_train = [class_to_idx[c] for c in train_set['tier2']]
+    y3_train = [class_to_idx[c] for c in train_set['tier3']]
+    y1_eval = [class_to_idx[c] for c in eval_set['tier1']]
+    y2_eval = [class_to_idx[c] for c in eval_set['tier2']]
+    y3_eval = [class_to_idx[c] for c in eval_set['tier3']]
+
+    # Instantiate the datasets
+    print('Tokenizing data...')
+    train_dataset = Tier3Dataset(train_set['description'], y1_train, y2_train, y3_train, tokenizer)
+    eval_dataset = Tier3Dataset(eval_set['description'], y1_eval, y2_eval, y3_eval, tokenizer)
+    print('Tokenization finished')
+
+    # Setting up the trainer
+    data_collator = HierarchicalDataCollator(tokenizer)
+    training_args = TrainingArguments(output_dir = 'roberta-t1-genres',
+                                      eval_strategy = 'epoch',
+                                      save_strategy = 'epoch')
+    trainer = Trainer(
+        model = model,
+        args = training_args,
+        data_collator= data_collator,
+        train_dataset = train_dataset,
+        eval_dataset = eval_dataset,
+        compute_metrics = compute_metrics
+        )
+    
+    trainer.train()
+
+    model.config.id2label = idx_to_class['tier1']
+    model.config.label2id = class_to_idx['tier1']
+
+    return model, tokenizer
+
 def main():
     download_dataset()
 
     train_set = parse_dataset('/content/data/BlurbGenreCollection_EN_train.txt')
     eval_set = parse_dataset('/content/data/BlurbGenreCollection_EN_dev.txt')
 
-    model, tokenizer = train_t1(train_set, eval_set)
+    model, tokenizer = train(train_set, eval_set)
 
     save_model('models', model, tokenizer)
 
--- trainer_utils.py
+++ trainer_utils.py
@@ -1,4 +1,7 @@
 import os
+import numpy as np
+import torch
+from transformers import DataCollatorWithPadding
 from sklearn.metrics import accuracy_score, f1_score
 
 def compute_metrics_t1(p):
@@ -11,6 +14,67 @@ def compute_metrics_t1(p):
         'f1-score': f1
     }
 
+def compute_metrics(eval_pred):
+    """
+    Calculates accuracy and F1-score for all three tiers.
+    """
+    predictions, labels = eval_pred
+    tier1_logits, tier2_logits, tier3_logits = predictions
+    tier1_labels, tier2_labels, tier3_labels = labels
+
+    tier1_preds = np.argmax(tier1_logits, axis=-1)
+    tier2_preds = np.argmax(tier2_logits, axis=-1)
+    tier3_preds = np.argmax(tier3_logits, axis=-1)
+
+    tier1_acc = accuracy_score(tier1_labels, tier1_preds)
+    tier2_acc = accuracy_score(tier2_labels, tier2_preds)
+    tier3_acc = accuracy_score(tier3_labels, tier3_preds)
+
+    tier1_f1 = f1_score(tier1_labels, tier1_preds, average = 'macro')
+    tier2_f1 = f1_score(tier2_labels, tier2_preds, average = 'macro')
+    tier3_f1 = f1_score(tier3_labels, tier3_preds, average = 'macro')
+
+    return {
+        "Tier 1 Accuracy": tier1_acc,
+        "Tier 2 Accuracy": tier2_acc,
+        "Tier 3 Accuracy": tier3_acc,
+        "Tier 1 F1": tier1_f1,
+        "Tier 2 F1": tier2_f1,
+        "Tier 3 F1": tier3_f1
+    }
+
+def get_mask_matrix(relations_map, parent_idxs, child_idxs):
+    """
+    Parses the parent-child relationships between two tiers of the hierarchy and turns them into a binary matrix.
+    Rows represent the higher tier.
+    Columns represents the lower tier.
+    The values within the matrix are binary, where 1 represents the existance and 0 the absence of a relationship.
+    """
+    mask = torch.zeros((len(parent_idxs), len(child_idxs)))
+
+    for parent, children in relations_map.items():
+        for child in children:
+            p_idx = parent_idxs[parent]
+            c_idx = child_idxs[child]
+            mask[p_idx, c_idx] = 1
+    return mask
+
+class HierarchicalDataCollator(DataCollatorWithPadding):
+    def __call__(self, features):
+        # Separate inputs and labels
+        inputs = [{k: v for k, v in f.items() if k not in ["tier1", "tier2", "tier3"]} for f in features]
+        tier1_labels = torch.tensor([f["t1_labels"] for f in features])
+        tier2_labels = torch.tensor([f["t2_labels"] for f in features])
+        tier3_labels = torch.tensor([f["t3_labels"] for f in features])
+
+        # Use the parent class to pad input fields
+        batch = super().__call__(inputs)
+        batch["t1_labels"] = tier1_labels
+        batch["t2_labels"] = tier2_labels
+        batch["t3_labels"] = tier3_labels
+
+        return batch
+
 def save_model(save_name, model, tokenizer):
     dir = 'models'
     save_path = os.path.join(dir, save_name)
@@ -18,4 +82,7 @@ def save_model(save_name, model, tokenizer):
     tokenizer_save_path = os.path.join(save_path, "tokenizer")
 
     model.save_pretrained(model_save_path)
+    print(f'Model saved to {model_save_path}')
+
     tokenizer.save_pretrained(tokenizer_save_path)
+    print(f'Tokenizer saved to {tokenizer_save_path}')
